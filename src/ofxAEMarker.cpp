40bbba611bb89568d38a0513f4d4957d15363783
nariakiiwatani <nariakiiwatani@annolab.com>
2014-04-29 20:13:52 +0900

    enabled layer comment

---
+++ src/ofxAEMarker.cpp
@@ -0,0 +1,35 @@
+#include "ofxAEMarker.h"
+
+OFX_AE_NAMESPACE_BEGIN
+
+void Marker::setupByComment(const string& com)
+{
+	size_t pos = com.find(comment::NEW_LINE);
+	if(pos == string::npos) {
+		name_ = com;
+		return;
+	}
+	name_ = com.substr(0, pos);
+	comment::extractParam(param_, com, pos+comment::NEW_LINE.length());
+	loop_ = FrameCounter::LOOP_NONE;
+	const string& loop = getParam("loop");
+	if(loop != "") {
+		if(loop == "oneway") {
+			loop_ = FrameCounter::LOOP_ONEWAY;
+		}
+		else if(loop == "pingpong") {
+			loop_ = FrameCounter::LOOP_PINGPONG;
+		}
+		else if(loop == "random") {
+			loop_ = FrameCounter::LOOP_RANDOM;
+		}
+	}
+}
+
+const string& Marker::getParam(const string& key)
+{
+	return comment::getParam(param_, key);
+}
+
+OFX_AE_NAMESPACE_END
+/* EOF */
